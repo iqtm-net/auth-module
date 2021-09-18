e7f8b84ce836848c037aff79c940cf35ecdf358e
iqtm-net <github@iqtm.net>
2021-09-18 15:49:07 +0300

    V0.3

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@
 | Here is where you can register all of the routes for an application.
 | It is a breeze. Simply tell Lumen the URIs it should respond to
 | and give it the Closure to call when that URI is requested.
-|
+| 
 */
 
 //PUBLIC
